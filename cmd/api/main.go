7704872206ac5a7c467dbcee2860d2f0a97c0349
Reptiled2 <ardaemek41@gmail.com>
2026-03-08 17:19:19 +0300

    Initial commit

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -49,7 +50,7 @@ func main() {
 
 	log.Printf("Server is listening on %s", server.Addr)
 	err := server.ListenAndServe()
-	if err != nil && err != http.ErrServerClosed {
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		panic(fmt.Sprintf("http server error: %s", err))
 	}
 
