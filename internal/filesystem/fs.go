2dfcb2ddd59e67b3c8772ed2e37e247736a45afc
Reptiled2 <ardaemek41@gmail.com>
2026-03-08 23:49:46 +0300

    Enhance FileManager with working directory support and additional file operations

--- internal/filesystem/fs.go
+++ internal/filesystem/fs.go
@@ -6,24 +6,84 @@ import (
 )
 
 type FileManager struct {
+	workingDir string
 }
 
 var (
-	fmInstance *FileManager
+	fmInstance *FileManager = nil
 	once       sync.Once
 )
 
-func New() *FileManager {
+func New(path string) (*FileManager, error) {
+	err := os.MkdirAll(path, 0750)
+	if err != nil && !os.IsExist(err) {
+		return nil, err
+	}
+
 	once.Do(func() {
-		fmInstance = &FileManager{}
+		fmInstance = &FileManager{
+			path,
+		}
 	})
 
+	return fmInstance, nil
+}
+
+func GetInstance() *FileManager {
 	return fmInstance
 }
 
-func (fm *FileManager) CreateFile(filePath string, ch chan<- error) error {
-	_, err := os.Create(filePath)
+func (fm *FileManager) GetWorkingDir() string {
+	return fm.workingDir
+}
 
-	ch <- err
-	return err
+func (fm *FileManager) CreateFile(fileName string) (*os.File, error) {
+	path, err := fm.joinPath(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_EXCL, 0644)
+}
+
+func (fm *FileManager) GetFile(fileName string) (*os.File, error) {
+	path, err := fm.joinPath(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(path, os.O_RDONLY, 0644)
+}
+
+func (fm *FileManager) IsExist(fileName string) (bool, error) {
+	path, err := fm.joinPath(fileName)
+	if err != nil {
+		return false, err
+	}
+
+	if _, err := os.Stat(path); err == nil {
+		return true, nil
+	} else if os.IsNotExist(err) {
+		return false, nil
+	} else {
+		return false, err
+	}
+}
+
+func (fm *FileManager) DeleteFile(fileName string) error {
+	path, err := fm.joinPath(fileName)
+	if err != nil {
+		return err
+	}
+
+	return os.Remove(path)
+}
+
+func (fm *FileManager) GetFileInfo(fileName string) (os.FileInfo, error) {
+	path, err := fm.joinPath(fileName)
+	if err != nil {
+		return nil, err
+	}
+
+	return os.Stat(path)
 }
