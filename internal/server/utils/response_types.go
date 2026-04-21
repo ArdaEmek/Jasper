bd377f87ff40cc99110adeaa3e7b61e00ac48b00
Reptiled2 <ardaemek41@gmail.com>
2026-04-21 20:49:49 +0300

    Add CreateMultipartUploadResponse type and refactor S3Response for improved error handling

---
+++ internal/server/utils/response_types.go
@@ -0,0 +1,10 @@
+package utils
+
+import "encoding/xml"
+
+type CreateMultipartUploadResponse struct {
+	XMLName  xml.Name `xml:"http://s3.amazonaws.com/doc/2006-03-01/ InitiateMultipartUploadResult"`
+	Bucket   string   `xml:"Bucket"`
+	Key      string   `xml:"Key"`
+	UploadId string   `xml:"UploadId"`
+}
